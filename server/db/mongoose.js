7d4dfdb412fd67096adadabd16635c777dee5b36
kalyanjyothula <kalyanjyothula@gmail.com>
2019-04-02 22:21:31 +0530

    api login added

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/Sampledb");
-
+mongoose.Promise = global.Promise;
 module.exports = {
   mongoose
 };
--- server/models/user.js
+++ server/models/user.js
@@ -51,41 +51,57 @@ userSchema.methods.generateAuthToken = function() {
 userSchema.methods.toJSON = function() {
   const user = this;
   // const userObject = user.toObject();
-  return _.pick(user, ['email', '_id']);
-}
+  return _.pick(user, ["email", "_id"]);
+};
 
 userSchema.statics.findByToken = function(token) {
   let user = this;
   let decoded;
-   try{
-     decoded = jwt.verify(token,"secret:over-flow")
-   }
-   catch(err) {
-    console.log('error',err)
-   }
-   console.log('user', decoded);
-   return user.findOne({
-     _id: decoded._id,
-     'tokens.token': token,
-     'tokens.access': 'auth',
-   });
-}
+  try {
+    decoded = jwt.verify(token, "secret:over-flow");
+  } catch (err) {
+    console.log("error", err);
+  }
+  console.log("user", decoded);
+  return user.findOne({
+    _id: decoded._id,
+    "tokens.token": token,
+    "tokens.access": "auth"
+  });
+};
 
-userSchema.pre('save', function(next){
+
+
+userSchema.pre("save", function(next) {
   const user = this;
-  if(user.isModified('password')){
+  if (user.isModified("password")) {
     bcrypt.genSalt(10, (err, salt) => {
       bcrypt.hash(user.password, salt, (err, hash) => {
         user.password = hash;
         next();
       });
-    })
-  }else {
+    });
+  } else {
     next();
   }
-})
- 
-const users = mongoose.model("users", userSchema);
+});
 
+userSchema.statics.findByCredentials = function(email, password) {
+  const user = this;
+  return user.findOne({ email }).then(data => {
+    if (!data) return Promise.reject();
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, data.password, (err, res) => {
+        if (res) {
+          resolve(data);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
+const users = mongoose.model("users", userSchema);
 
 module.exports = { users };
